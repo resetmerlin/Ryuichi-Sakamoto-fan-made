c38c90a7fa2aec1eaf21eea9a5b5d42c93849372
resetmerlin <resetmerlin@gmail.com>
2023-12-11 13:17:39 +0900

    feat: Add HomePage

---
+++ client/src/page/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Donut, Header } from '../../components';
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+
+      <main id="container">
+        <Donut />
+      </main>
+    </>
+  );
+}
