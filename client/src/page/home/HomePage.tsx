dfda699149f0dbd3b48204a9f9036ba6f0d5bb82
resetmerlin <resetmerlin@gmail.com>
2023-12-11 18:22:05 +0900

    feat: Add HomePage module scss file

--- client/src/page/home/HomePage.tsx
+++ client/src/page/home/HomePage.tsx
@@ -1,12 +1,15 @@
-import { Donut, Header } from '../../components';
+import { Description, Donut, Header } from '../../components';
+import styles from './HomePage.module.scss';
 
 export default function HomePage() {
   return (
     <>
       <Header />
-
       <main id="container">
-        <Donut />
+        <section className={styles.home}>
+          <Donut />
+          <Description />
+        </section>
       </main>
     </>
   );
