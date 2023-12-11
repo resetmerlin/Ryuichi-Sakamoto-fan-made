71f76e9ebeb8ab176394463ff6d005df534c85cf
resetmerlin <resetmerlin@gmail.com>
2023-12-11 13:16:00 +0900

    feat: Add Header component

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+
+export default function Header() {
+  return (
+    <div id="header" className={styles.header}>
+      <div className={styles.header__left}>
+        <Link to="/">Ryuichi Sakamoto</Link>
+      </div>
+
+      <div className={styles.header__right}>
+        <Link to="/songs">Songs</Link>
+        <Link to="/">About</Link>
+        <Link to="/">How to use</Link>
+        <Link to="/">Services</Link>
+      </div>
+    </div>
+  );
+}
