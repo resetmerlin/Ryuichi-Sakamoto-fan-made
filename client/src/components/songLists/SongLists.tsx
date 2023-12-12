95a22bc4e59174dbea35b99f535cf147b53f7c77
resetmerlin <resetmerlin@gmail.com>
2023-12-12 11:27:03 +0900

    feat: Add mapping on SongRow component

--- client/src/components/songLists/SongLists.tsx
+++ client/src/components/songLists/SongLists.tsx
@@ -1,11 +1,17 @@
+import { ISong, ISongs } from '../../types';
 import styles from './SongLists.module.scss';
 import { SongRow } from './songRow';
 
-export default function SongLists() {
+type IProps = {
+  songs: ISongs;
+};
+
+export default function SongLists({ songs }: IProps) {
   return (
     <div className={styles.songLists}>
       <span className={styles.songLists__title}>TRENDING SONGS</span>
-      <SongRow />
+
+      {songs && songs.map((song: ISong) => <SongRow song={song} />)}
     </div>
   );
 }
