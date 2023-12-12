cac11f3668ec5f6bc02caefaa8a2370710f90ee5
resetmerlin <resetmerlin@gmail.com>
2023-12-12 10:02:00 +0900

    feat: Add Song Page on Router

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,10 +1,15 @@
 import { createHashRouter } from 'react-router-dom';
 import { HomePage } from '../page';
+import { SongPage } from '../page/song';
 
 const router = createHashRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/songs',
+    element: <SongPage />,
   },
 ]);
 
