704115d6602f5c3eb1b0fd5cfea3bdc0e296e197
resetmerlin <resetmerlin@gmail.com>
2023-12-12 10:02:26 +0900

    feat: Add Song Page

---
+++ client/src/page/song/SongPage.tsx
@@ -0,0 +1,46 @@
+import { Layout, SongLists } from '../../components';
+import { Song } from '../../components/song';
+import styles from '../home/HomePage.module.scss';
+
+export default function SongPage() {
+  const musicLists = [
+    {
+      img: '/song1.jpg',
+      name: 'Amore',
+      artist: 'Ryuichi Sakamoto',
+      music: 'music/Amore.mp3',
+    },
+    {
+      img: '/song2.jpg',
+      name: 'Thousand knvies',
+      artist: 'Ryuichi Sakamoto',
+      music: 'music/Thousand Knives.mp3',
+    },
+    {
+      img: '/song3.jpg',
+      name: 'Energy flow',
+      artist: 'Ryuichi Sakamoto',
+      music: 'music/Energy flow.mp3',
+    },
+    {
+      img: '/song4.jpg',
+      name: 'Merry Christmas Mr.Lawrence',
+      artist: 'Ryuichi Sakamoto',
+      music: 'music/Merry Christmas Mr. Lawrence.mp3',
+    },
+
+    {
+      img: '/song5.jpg',
+      name: 'Shining Boy & Little Randy',
+      artist: 'Ryuichi Sakamoto',
+      music: 'music/Shining Boy & Little Randy.mp3',
+    },
+  ];
+  return (
+    <Layout>
+      <Song.Section>
+        <SongLists />
+      </Song.Section>
+    </Layout>
+  );
+}
