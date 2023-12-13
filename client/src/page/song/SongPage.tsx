ad510661867d7bfc620054f7506ca80809df54f0
resetmerlin <resetmerlin@gmail.com>
2023-12-13 13:37:51 +0900

    feat: Add feature music play, pause, change volume, change duration

--- client/src/page/song/SongPage.tsx
+++ client/src/page/song/SongPage.tsx
@@ -1,45 +1,59 @@
-import { Layout, Phone, SongLists } from '../../components';
-import { Song } from '../../components/song';
+import { useRef, useState } from 'react';
+import { Layout, Phone, Song, SongLists } from '../../components';
+import { musicLists } from '../../lib';
+import { ISong } from '../../types';
 
 export default function SongPage() {
-  const musicLists = [
-    {
-      img: '/song1.jpg',
-      name: 'Amore',
-      artist: 'Ryuichi Sakamoto',
-      music: 'music/Amore.mp3',
-    },
-    {
-      img: '/song2.jpg',
-      name: 'Thousand knvies',
-      artist: 'Ryuichi Sakamoto',
-      music: 'music/Thousand Knives.mp3',
-    },
-    {
-      img: '/song3.jpg',
-      name: 'Energy flow',
-      artist: 'Ryuichi Sakamoto',
-      music: 'music/Energy flow.mp3',
-    },
-    {
-      img: '/song4.jpg',
-      name: 'Merry Christmas Mr.Lawrence',
-      artist: 'Ryuichi Sakamoto',
-      music: 'music/Merry Christmas Mr. Lawrence.mp3',
-    },
+  const [currSong, setCurrSong] = useState<ISong | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-    {
-      img: '/song5.jpg',
-      name: 'Shining Boy & Little Randy',
-      artist: 'Ryuichi Sakamoto',
-      music: 'music/Shining Boy & Little Randy.mp3',
-    },
-  ];
+  const musicRef = useRef(new Audio());
+  const durationToggleRef = useRef<HTMLInputElement | null>(null);
+
+  const playMusic = (song: ISong) => {
+    if (durationToggleRef.current) durationToggleRef.current.value = '0';
+    musicRef.current.pause();
+    setIsPlaying(false);
+
+    setCurrSong(song);
+
+    musicRef.current = new Audio(`../${song?.music}`);
+    musicRef.current.play();
+    setIsPlaying(true);
+  };
+
+  const pauseMusic = () => {
+    if (musicRef.current.duration > 0 && !musicRef.current.paused) {
+      musicRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      musicRef.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const changeVolumneMusic = (e: React.FormEvent<HTMLInputElement>) => {
+    musicRef.current.volume = +e.currentTarget.value / 100;
+  };
+
+  const changeDurationMusic = (e: React.FormEvent<HTMLInputElement>) => {
+    const seekTime = (musicRef.current.duration / 100) * +e.currentTarget.value;
+
+    musicRef.current.currentTime = seekTime;
+  };
+
   return (
     <Layout>
       <Song.Section>
-        <SongLists songs={musicLists} />
-        <Phone />
+        <SongLists songs={musicLists} playMusic={playMusic} />
+        <Phone
+          currSong={currSong}
+          pauseMusic={pauseMusic}
+          isPlaying={isPlaying}
+          changeVolumneMusic={changeVolumneMusic}
+          changeDurationMusic={changeDurationMusic}
+          ref={durationToggleRef}
+        />
       </Song.Section>
     </Layout>
   );
