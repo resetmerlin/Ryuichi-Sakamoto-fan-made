983fab6909db77099a8d7c8d2c91bed5ecfcba76
resetmerlin <resetmerlin@gmail.com>
2023-12-13 13:36:45 +0900

    feat: Add dummy musicLists data

---
+++ client/src/lib/dummyData.ts
@@ -0,0 +1,33 @@
+export const musicLists = [
+  {
+    img: '/song1.jpg',
+    name: 'Amore',
+    artist: 'Ryuichi Sakamoto',
+    music: 'music/Amore.mp3',
+  },
+  {
+    img: '/song2.jpg',
+    name: 'Thousand knvies',
+    artist: 'Ryuichi Sakamoto',
+    music: 'music/Thousand Knives.mp3',
+  },
+  {
+    img: '/song3.jpg',
+    name: 'Energy flow',
+    artist: 'Ryuichi Sakamoto',
+    music: 'music/Energy flow.mp3',
+  },
+  {
+    img: '/song4.jpg',
+    name: 'Merry Christmas Mr.Lawrence',
+    artist: 'Ryuichi Sakamoto',
+    music: 'music/Merry Christmas Mr. Lawrence.mp3',
+  },
+
+  {
+    img: '/song5.jpg',
+    name: 'Shining Boy & Little Randy',
+    artist: 'Ryuichi Sakamoto',
+    music: 'music/Shining Boy & Little Randy.mp3',
+  },
+];
