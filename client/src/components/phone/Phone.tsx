8677d7d3c811f6592392e9bb6ab25c426c961adc
resetmerlin <resetmerlin@gmail.com>
2023-12-13 13:34:24 +0900

    feat: Add volume, duration range of Phone

--- client/src/components/phone/Phone.tsx
+++ client/src/components/phone/Phone.tsx
@@ -1,6 +1,19 @@
+import { forwardRef } from 'react';
+import { ISong } from '../../types';
 import './Phone.scss';
 
-export default function Phone() {
+type IProps = {
+  currSong: ISong | null;
+  pauseMusic: () => void;
+  isPlaying: boolean;
+  changeVolumneMusic: (e: React.FormEvent<HTMLInputElement>) => void;
+  changeDurationMusic: (e: React.FormEvent<HTMLInputElement>) => void;
+};
+
+export default forwardRef<HTMLInputElement, IProps>(function Phone(
+  { currSong, pauseMusic, isPlaying, changeVolumneMusic, changeDurationMusic },
+  ref
+) {
   return (
     <div className="outside-border">
       <div className="silencer"></div>
@@ -8,18 +21,6 @@ export default function Phone() {
       <div className="volume-down"></div>
       <div className="button-on"></div>
       <div className="inside-border">
-        <div className="camera">
-          <div className="camera-dot">
-            <div className="camera-dot-2"></div>
-            <div className="camera-dot-3"></div>
-          </div>
-          <div className="camera-speaker"></div>
-        </div>
-
-        <div className="lock">
-          <div className="lock-locked"></div>
-        </div>
-
         <div className="time">19:53</div>
 
         <div className="t-r-info">
@@ -32,25 +33,55 @@ export default function Phone() {
 
         <div className="date">Tuesday, 9 August</div>
 
-        <div className="torch-outter">
-          <div className="light"></div>
-          <div className="top"></div>
-          <div className="switch-top"></div>
-          <div className="switch-section"></div>
-          <div className="switch">
-            <div className="dot"></div>
-          </div>
+        <div className="button__wrapper">
+          <button>
+            <i className="bx bx-skip-previous"></i>
+          </button>
+          <button onClick={pauseMusic}>
+            <i
+              className={`bx bx-play-circle ${!isPlaying ? 'none' : 'flex'}`}
+            ></i>
+            <i
+              className={`bx bx-pause-circle ${!isPlaying ? 'flex' : 'none'}`}
+            ></i>
+          </button>
+          <button>
+            <i className="bx bx-skip-next"></i>
+          </button>
         </div>
 
-        <div className="camera-outter">
-          <div className="box"></div>
-          <div className="eye"></div>
-          <div className="circle"></div>
-          <div className="dot"></div>
+        <div className="music__content__wrapper ">
+          <div className={`music__content ${isPlaying ? 'rotate' : ''}`}>
+            <img src={currSong?.img} alt="song1" />
+          </div>
+
+          <span>{currSong?.name}</span>
+          <span>{currSong?.artist}</span>
+
+          <div className="slider__wrapper">
+            <i className="bx bxs-volume-low"></i>
+            <input
+              type="range"
+              min={1}
+              max={100}
+              className="slider volumne__slider"
+              onChange={changeVolumneMusic}
+            />
+            <i className="bx bxs-volume-full"></i>
+          </div>
+
+          <input
+            type="range"
+            min={1}
+            max={100}
+            className="slider length__slider"
+            onChange={changeDurationMusic}
+            ref={ref}
+          />
         </div>
 
         <div className="bottom-line"></div>
       </div>
     </div>
   );
-}
+});
