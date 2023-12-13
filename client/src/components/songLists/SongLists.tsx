6e6addb60dd9c3237189f61399e14c911e15d473
resetmerlin <resetmerlin@gmail.com>
2023-12-13 13:35:25 +0900

    feat: Add key and playMusic props on Song Row component

--- client/src/components/songLists/SongLists.tsx
+++ client/src/components/songLists/SongLists.tsx
@@ -4,14 +4,22 @@ import { SongRow } from './songRow';
 
 type IProps = {
   songs: ISongs;
+  playMusic: (song: ISong) => void;
 };
 
-export default function SongLists({ songs }: IProps) {
+export default function SongLists({ songs, playMusic }: IProps) {
   return (
     <div className={styles.songLists}>
       <span className={styles.songLists__title}>TRENDING SONGS</span>
 
-      {songs && songs.map((song: ISong) => <SongRow song={song} />)}
+      {songs &&
+        songs.map((song: ISong) => (
+          <SongRow
+            song={song}
+            playMusic={playMusic}
+            key={`${song.artist} + ${song.name}`}
+          />
+        ))}
     </div>
   );
 }
