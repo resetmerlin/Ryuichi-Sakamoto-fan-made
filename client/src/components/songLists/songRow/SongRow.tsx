4b6f88da8b0cd345e9cbc6e0eb1c97b396fc9801
resetmerlin <resetmerlin@gmail.com>
2023-12-13 13:36:23 +0900

    feat: Add playmusic callback function on onClick button

--- client/src/components/songLists/songRow/SongRow.tsx
+++ client/src/components/songLists/songRow/SongRow.tsx
@@ -3,10 +3,11 @@ import styles from './SongRow.module.scss';
 
 type IProps = {
   song: ISong;
+  playMusic: (song: ISong) => void;
 };
-export default function SongRow({ song }: IProps) {
+export default function SongRow({ song, playMusic }: IProps) {
   return (
-    <button className={styles.songRow}>
+    <button className={styles.songRow} onClick={() => playMusic(song)}>
       <img src={song?.img} alt="song1" />
       <div className={styles.songRow__text}>
         <span>{song?.name}</span>
